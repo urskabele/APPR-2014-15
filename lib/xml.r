88f9391bb75261c7ff6d0399b8c5ac9bc06d28d1
Urska Bele <urska.bele1@gmail.com>
2014-12-06 14:45:17 +0100

    ..

--- lib/xml.r
+++ lib/xml.r
@@ -9,30 +9,40 @@ stripByPath <- function(x, path) {
                     function(y) gsub("^\\s*(.*?)\\s*$", "\\1", xmlValue(y))))
 }
 
-uvozi.oi <- function() {
-  url.oi <- "http://en.wikipedia.org/wiki/List_of_Olympic_Games_host_cities"
-  doc.oi <- htmlTreeParse(url.oi, useInternalNodes=TRUE)
+uvozi.obcine <- function() {
+  url.obcine <- "http://en.wikipedia.org/wiki/List_of_Olympic_Games_host_cities"
+  doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
   
   # Poiščemo vse tabele v dokumentu
-  tabele <- getNodeSet(doc.oi, "//table")
+  tabele <- getNodeSet(doc.obcine, "//table")
   
   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
   # trenutnim vozliščem
-  vrstice <- getNodeSet(tabele[[2]], "./tr")
+  vrstice <- getNodeSet(tabele[[1]], "./tr")
   
   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
   seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
   
+  # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
+  # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
+  seznam <- lapply(seznam, function(x)
+    if (length(x) == 9) {
+      return(x)
+    } else {
+      return(c(x, ""))
+    })
+  
   # Iz seznama vrstic naredimo matriko
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
   
   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
-  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[1]][[1]], ".//th"))
   
   # Podatke iz matrike spravimo v razpredelnico
-  return(data.frame(apply(gsub("\\*", "",
-                          gsub(",", ".",
-                          gsub("\\.", "", matrika[,2:5]))),
-                    2, as.numeric), row.names=matrika[,1]))
+  tabela <- data.frame(gsub("\\[.*$", "", matrika), stringsAsFactors=FALSE)
+  tabela$Year <- as.numeric(as.character(tabela$Year))
+  row.names(tabela) <- ifelse(tabela$Summer == "—", tabela$Winter, tabela$Summer)
+  tabela<-tabela[,-(4:5)]
+  return(tabela)
 }
