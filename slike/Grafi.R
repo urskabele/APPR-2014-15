15ce758433116c16ff65d361c740b285ae196b7c
Urska Bele <urska.bele1@gmail.com>
2014-12-07 19:05:21 +0100

    attach detach

--- slike/Grafi.R
+++ slike/Grafi.R
@@ -6,6 +6,6 @@ plot(OI$Leto[Vrsta=="poletne"],OI$Stevilo.udelezenk[Vrsta=="poletne"],xlab="Leto
 
 #Stevilo udelezenk zimskih OI
 plot(OI$Leto[Vrsta=="zimske"],OI$Stevilo.udelezenk[Vrsta=="zimske"],xlab="Leto",ylab="Število udeleženk",main="Število držav udeleženk na zimskih OI",pch=20,col="blue",type="l",lwd=3.5)
-
+detach(OI)
 dev.off()
 
