7b0a08ff6d5f8cc79caed91fc4f9b5c5871b6e1a
Urska Bele <urska.bele1@gmail.com>
2015-01-08 20:21:36 +0100

    Končan zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -57,11 +57,20 @@ plot(svet, col = barve)
 title("Število športnikov iz posameznih držav na OI 2012")
 legend("left", legend = round(seq(min.sportniki, max.sportniki, (max.sportniki-min.sportniki)/5)),
        fill = rgb(1, 0, 0, (1:6)/6), bg = "white")
-#Oznacimo nekatera mesta
-oznake<-data.frame(E = c(76,-74), N = c(60,40))
-points(coordinates(oznake),pch=20)
 
-#Imena drzav
-#text(oznake)
+#Oznacimo nekatera mesta: NY, Peking, London, Sydney, Moskva,Johannesburg, Rio de Janeiro
+oznake<-data.frame(E = c(-74,116,0,151,37,28,-43), N = c(40,40,51,-33,55,-26,-22))
+points(coordinates(oznake),pch=20,cex=0.7,col="blue")
+#dodamo imena mest
+text(coordinates(oznake[c(1,4,5,7),]),labels=c("NY","Sydney","Moskva","Rio de Janeiro"),pos=4,cex=0.5,offset=0.15,col="blue")
+text(coordinates(oznake[2,]),labels=c("Peking"),pos=3,cex=0.5,offset=0.15,col="blue")
+text(coordinates(oznake[3,]),labels=c("London"),pos=2,cex=0.5,offset=0.8,col="blue")
+text(coordinates(oznake[6,]),labels=c("Johannesburg"),pos=1,cex=0.5,offset=0.8,col="blue")
+
+#Dodamo imena drzav
+#najprej poberemo iz zemljevida svet tiste države ki bi jih radi označili
+oznacene<-svet[c(9,23,28,31,136,169),]
+#dodamo imena na zemljevid
+text(coordinates(oznacene),labels=c("Avstralija","Brazilija","Kanada","Kitajska","Rusija","ZDA"),cex=0.55,col="black")
 
 dev.off()
