9a2b287894c95ef441bcb5ddfb8241501f9d3368
Urska Bele <urska.bele1@gmail.com>
2015-02-22 16:57:45 +0100

    Drugi del 4.faze - prileganja in napovedi.

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -60,3 +60,61 @@ oznacene<-svet[c(9,23,28,31,136,169),]
 text(coordinates(oznacene),labels=c("Avstralija","Brazilija","Kanada","Kitajska","Rusija","ZDA"),cex=0.55,col="black")
 
 dev.off()
+
+
+#2.) Krivulje, ki se najbolj prilegajo številu dogodkov na poletnih in zimskih OI
+#attach(OI)
+pdf("slike/naprednigrafi.pdf",paper="a4r")
+
+
+#2.1.)modeli: krivulje za št dogodkov poletnih OI
+
+letop<-OI$Leto[Vrsta=="poletne"] #leta poletnih OI
+dogp<-OI$Stevilo.dogodkov[Vrsta=="poletne"] #st dogodkov poletnih OI
+
+   #graf
+plot(letop,dogp, xlim=c(1896,2050),ylim=c(0,500),
+     xlab="Leto",ylab="Število dogodkov",
+     main="Napoved za število dogodkov na poletnih OI",pch=20,col="red",type="p",lwd=3.5)
+
+   #premica
+linp<-lm(dogp~letop)
+abline(linp,col="green")
+   #parabola
+kvp<-lm(dogp~I(letop^2)+letop)
+curve(predict(kvp, data.frame(letop=x)), add = TRUE, col = "purple")
+   #loess
+loep<-loess(dogp~letop)
+curve(predict(loep, data.frame(letop=x)),add=TRUE,col="orange")
+   #legenda
+legend("topleft", c("Linerana metoda", "Kvadratna metoda","Loess"),lty=c(1,1,1), col = c("green","purple","orange"))
+   #Ocenimo prileganje krivulj tako, da izračunamo vsote kvadratov razdalj od napovedanih do dejanskih vrednosti
+ostp<-sapply(list(linp, kvp, loep), function(x) sum(x$residuals^2))
+
+#2.2.)modeli: krivulje za št dogodkov zimskih OI
+
+letoz<-OI$Leto[Vrsta=="zimske"] #leta zimskih OI
+dogz<-OI$Stevilo.dogodkov[Vrsta=="zimske"] #st dogodkov zimskih OI
+
+   #graf
+plot(letoz,dogz,xlim=c(1924,2050),ylim=c(0,200),
+     xlab="Leto",ylab="Število dogodkov",
+     main="Napoved za število dogodkov na zimskih OI",pch=20,col="blue",type="p",lwd=3.5)
+   
+   #premica
+linz<-lm(dogz~letoz)
+abline(linz,col="green")
+   #parabola
+kvz<-lm(dogz~I(letoz^2)+letoz)
+curve(predict(kvz, data.frame(letoz=x)), add = TRUE, col = "purple")
+   #loess
+loez<-loess(dogz~letoz)
+curve(predict(loez, data.frame(letoz=x)),add=TRUE,col="orange")
+   #legenda
+legend("topleft", c("Linerana metoda", "Kvadratna metoda","Loess"),lty=c(1,1,1), col = c("green","purple","orange"))
+   #Ocenimo prileganje krivulj tako, da izračunamo vsote kvadratov razdalj od napovedanih do dejanskih vrednosti
+ostz<-sapply(list(linz, kvz, loez), function(x) sum(x$residuals^2))
+
+#detach(OI)
+dev.off()
+
